edf3aab7281cd41af99b0b360cd393a01b067ed4
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-01-26 03:04:42 +0530

    added splash screen

---
+++ frontend/constants/app.constants.jsx
@@ -0,0 +1,4 @@
+export const APP_NAME = 'Deca-Org';
+export const APP_SUBTITLE = 'A decentralized solution to team management! Let\'s get started!';
+export const APP_DESCRIPTION = 'Deca-Org is a decentralized solution to team management. It is a web application that allows users to create and manage teams, and to assign tasks to team members.';
+export const APP_TAGS = 'DAO\'s - MEET - CHAT - SHARE';
