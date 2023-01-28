d8e6c5e1a8dbc3e3cd8ee1b188cf1d5c96f3b93c
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2023-01-29 02:30:17 +0530

    recording issue solved

--- frontend/components/huddle/Recordings.jsx
+++ frontend/components/huddle/Recordings.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Recordings = ({ recordings }) => {
   return (
     <div>
-        <h1>Recordings</h1>
+      <h1>Recordings</h1>
       <table className="table w-fit">
         <thead>
           <tr>
@@ -16,18 +16,20 @@ const Recordings = ({ recordings }) => {
           </tr>
         </thead>
       </table>
-        {recordings.map((recording, i) => {
-          console.log(typeof recording);
-          <div> <Link href={recording.url}> Download now! </Link> with video of {recording.duration/1000}s and {recording.size} Mb </div>
-          {/* <div key={i}>
+      {recordings.map((recording, i) => {
+        return (
+          <div key={i}>
             <tr>
               <th>{i + 1}</th>
-              <td><Link href={recording.url}> Download now! </Link> </td>
-              <td>{recording.duration/1000}s</td>
+              <td>
+                <Link href={recording.url}> Download now! </Link>{" "}
+              </td>
+              <td>{recording.duration / 1000}s</td>
               <td>{recording.size} Mb</td>
             </tr>
-          </div>; */}
-        })}
+          </div>
+        );
+      })}
     </div>
   );
 };
