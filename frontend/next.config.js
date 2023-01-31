12b28505686cb9586eb16471fbd70c33a07ccc42
roohan25 <rohansasne30@gmail.com>
2023-01-31 15:17:01 +0530

    Spheron Next.js Config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
+
 }
 
 module.exports = nextConfig
