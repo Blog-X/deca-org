13e29193b8e50320443c33330ee2ab9e5e52ad0d
roohan25 <rohansasne30@gmail.com>
2023-02-05 03:30:04 +0530

    test Branch

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   images: {
     loader: 'akamai',
     path: '',
   },
   trailingSlash: true,
-=======
->>>>>>> 9a940eebe20af96420e744f393a5cbb3eeeed187
 
 }
 
